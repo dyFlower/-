88b2c28a8e704f8f20e728b6c08ed11d46b7531d
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-09-02 01:36:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -8,8 +8,11 @@ function solution(n, m) {
         n = a % b
         m = b
     }
-    answer.push(m)
-    answer.push(k / m)
+    answer.push(m, k / m)
     
     return answer;
 }
+
+/*유클리드 호제법 사용. 나머지와의 최대공약수를 구하면 그게 최대공약수이다.
+그러므로 계속해서 나누다가 0이되면 나눈 수가 최대공약수.
+최대공약수 * 최대공배수는 m * n인 것을 이용하면 쉽게 구할 수 있다.*/
