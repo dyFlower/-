a4fb7fddddf0be3260f6998a0b05fa3a0c4f19f5
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-09-03 17:57:36 +0900

    [level 1] Title: 체육복, Time: 3.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,36 @@
+function solution(n, lost, reserve) {
+    let student = new Array(n)
+    let cntL = 0;
+    //'l','r','y'
+
+    for(let i = 0; i < reserve.length; i++){
+        student[reserve[i] - 1] = 'r'
+    }
+    for(let i = 0; i < lost.length; i++){
+        if(student[lost[i] - 1] === 'r'){
+            student[lost[i] - 1] = 'y'
+        }else{
+        student[lost[i] - 1] = 'l'}
+    }
+    for(let i = 0; i < student.length; i++){
+        if(student[i] === 'l'){
+            if(student[i - 1] === 'r'){
+                student[i] = 'y'
+                student[i - 1] = 'y'
+            }
+            else if(student[i + 1] === 'r'){
+                student[i] = 'y'
+                student[i + 1] = 'y'
+            }
+        }
+    }
+    
+    for(let i = 0; i < student.length; i++){
+        if(student[i] === 'l'){
+            cntL++
+        }
+    }
+    console.log(student)
+    console.log(cntL)
+    return n - cntL;
+}
