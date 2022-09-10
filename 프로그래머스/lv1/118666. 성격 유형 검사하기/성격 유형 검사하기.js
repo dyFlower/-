0f50b494061d8813354b251c6a63cbb1cbff04c9
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-09-10 17:14:34 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.73 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,21 @@
+function solution(survey, choices) {
+    var answer = '';
+    let surScore = {R : 0, T : 0, C : 0, F : 0, J : 0, M : 0, A : 0,
+                    N : 0}
+    let box = [];
+    
+    for(let i = 0; i < choices.length; i++){
+        choices[i] -= 4
+    }
+    for(let i = 0; i < survey.length; i++){
+        box = survey[i].split('')
+        surScore[box[1]] += choices[i]
+    }
+    
+    surScore.T > surScore.R ? answer+='T' : answer+='R'
+    surScore.F > surScore.C ? answer+='F' : answer+='C'
+    surScore.M > surScore.J ? answer+='M' : answer+='J'
+    surScore.N > surScore.A ? answer+='N' : answer+='A'
+    
+    return answer;
+}
