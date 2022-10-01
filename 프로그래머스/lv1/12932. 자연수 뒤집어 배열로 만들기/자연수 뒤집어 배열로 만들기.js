773880795d77855449834394e0c86ab8370d9aaa
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-01 15:50:56 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,12 +1,13 @@
 function solution(n) {
-    var answer = [];
-    
-    let a = n.toString();
-    let b = a.split('');
-    console.log(b)
-    for(let i = 1; i < b.length + 1; i++){
-        answer.push(parseInt(b[b.length - i]))
-    }
-    
+    var answer = n.toString().split('').reverse().map(Number);
+
     return answer;
 }
+
+//     let a = n.toString();
+//     let b = a.split('');
+//     console.log(b)
+//     for(let i = 1; i < b.length + 1; i++){
+//         answer.push(parseInt(b[b.length - i]))
+//     }
+    
