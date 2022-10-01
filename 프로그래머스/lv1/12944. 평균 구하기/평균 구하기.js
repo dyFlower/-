7b0808513ed03eb076edc5850d58032d077ded52
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-01 15:48:32 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,8 +1,10 @@
 function solution(arr) {
-    var answer = 0;
-    for(let i = 0; i < arr.length; i++){
-        answer += arr[i];
-    }
-    answer = answer / arr.length;
+    var answer = arr.reduce((acc, arr) => acc + arr) / arr.length;
+
     return answer;
 }
+
+    // for(let i = 0; i < arr.length; i++){
+    //     answer += arr[i];
+    // }
+    // answer = answer / arr.length;
