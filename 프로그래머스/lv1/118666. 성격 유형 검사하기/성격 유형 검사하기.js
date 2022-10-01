f4fcc20f17fcb00e823b93c744ac958d52ed2ca5
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-01 15:38:15 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -2,18 +2,18 @@ function solution(survey, choices) {
     var answer = '';
     let surScore = {R : 0, T : 0, C : 0, F : 0, J : 0, M : 0, A : 0,
                     N : 0}
-    let box = [];
+    let tmp = [];
     
     choices.forEach((choice, i, a) => a[i] -= 4)
     
     survey.forEach((mbti, i, a) => {
-        box = a[i].split('');
-        surScore[box[1]] += choices[i]})
+        tmp = a[i].split('');
+        surScore[tmp[1]] += choices[i]})
     
-    surScore.T > surScore.R ? answer+='T' : answer+='R'
-    surScore.F > surScore.C ? answer+='F' : answer+='C'
-    surScore.M > surScore.J ? answer+='M' : answer+='J'
-    surScore.N > surScore.A ? answer+='N' : answer+='A'
+    answer += surScore.T > surScore.R ? 'T' : 'R'
+    answer += surScore.F > surScore.C ? 'F' : 'C'
+    answer += surScore.M > surScore.J ? 'M' : 'J'
+    answer += surScore.N > surScore.A ? 'N' : 'A'
     
     return answer;
 }
@@ -26,3 +26,6 @@ function solution(survey, choices) {
     //     box = survey[i].split('')
     //     surScore[box[1]] += choices[i]
     // }
+
+// 변수명 box -> tmp로 수정
+// 삼항 연산자 구문 좀 더 짧게 수정
