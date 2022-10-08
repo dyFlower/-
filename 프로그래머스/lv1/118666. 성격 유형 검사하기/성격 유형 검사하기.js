68b5947a7920b8ca7a820cebe97e66a8e54fdcc3
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-08 09:38:12 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,31 +1,4 @@
 function solution(survey, choices) {
     var answer = '';
-    let surScore = {R : 0, T : 0, C : 0, F : 0, J : 0, M : 0, A : 0,
-                    N : 0}
-    let tmp = [];
-    
-    choices.forEach((choice, i, a) => a[i] -= 4)
-    
-    survey.forEach((mbti, i, a) => {
-        tmp = a[i].split('');
-        surScore[tmp[1]] += choices[i]})
-    
-    answer += surScore.T > surScore.R ? 'T' : 'R'
-    answer += surScore.F > surScore.C ? 'F' : 'C'
-    answer += surScore.M > surScore.J ? 'M' : 'J'
-    answer += surScore.N > surScore.A ? 'N' : 'A'
-    
     return answer;
 }
-
-    // for(let i = 0; i < choices.length; i++){
-    //     choices[i] -= 4
-    // }
-
-    // for(let i = 0; i < survey.length; i++){
-    //     box = survey[i].split('')
-    //     surScore[box[1]] += choices[i]
-    // }
-
-// 변수명 box -> tmp로 수정
-// 삼항 연산자 구문 좀 더 짧게 수정
