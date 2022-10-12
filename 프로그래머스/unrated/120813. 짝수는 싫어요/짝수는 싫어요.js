b83097fa286ebceca94804e6055023760ad85a01
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-12 20:15:21 +0900

    [unrated] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,7 +1,9 @@
 function solution(n) {
-    var answer = new Array(n)
-    for(let i = 1; i <= n; i++){
-        answer[i] =i
-    }
-    return answer.filter(n => n%2 != 0);
+    return Array(n).fill(1).map((n, i)=> n+i).filter(n => n % 2 != 0)
 }
+
+//     var answer = new Array(n)
+//     for(let i = 1; i <= n; i++){
+//         answer[i] =i
+//     }
+//     return answer.filter(n => n%2 != 0);
