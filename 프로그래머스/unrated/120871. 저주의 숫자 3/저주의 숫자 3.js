5654c40c6b1d8a8575d68c602809d5ed01c6ee3f
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-16 20:13:52 +0900

    [unrated] Title: 저주의 숫자 3, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    
+    for(let i = 0; i < n; i++){
+        answer += 1
+        
+        while(answer.toString().split('').includes('3') || answer % 3 === 0){
+            answer += 1
+        }
+    }
+    return answer;
+}
