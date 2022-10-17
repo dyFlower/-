ad9d26eba5c16fff599bd62e73db88668ea9b906
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-17 21:40:54 +0900

    [unrated] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.js
@@ -1,14 +1,12 @@
 function solution(id_pw, db) {
-    var answer = 'fail';
-    
     for(let i = 0; i < db.length; i++){
         if(id_pw[0] === db[i][0]){
             if(id_pw[1] == db[i][1]){
-                answer = 'login'
+                return 'login'
             }else{
-                answer = 'wrong pw'
+                return 'wrong pw'
             }
         }
     }
-    return answer;
+    return 'fail';
 }
