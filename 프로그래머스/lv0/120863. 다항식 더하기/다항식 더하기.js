eeea5bc79c7d196f15b4463f9feaabc3c22789b4
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-20 04:37:28 +0900

    [level 0] Title: 다항식 더하기, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,21 @@
+function solution(polynomial) {
+    var answer = 0;
+    let temp = 0;
+    polynomial = polynomial.split(' ').filter(v => v !== '+')
+    for(i of polynomial){
+        if(i === 'x'){
+            temp += 1}
+        else if(i[i.length - 1] === 'x'){
+            temp += +i.substr(0, i.length - 1)}
+        else{answer += +i}
+    }
+        if(temp === 1){
+            if(answer === 0){
+                return 'x'
+            }
+        return `x + ${answer}`
+    }
+    answer = temp === 0 ? answer : answer === 0 ? temp + 'x' : `${temp}x + ${answer}` 
+                        
+    return answer.toString();
+}
