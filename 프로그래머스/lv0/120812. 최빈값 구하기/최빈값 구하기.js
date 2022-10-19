b33c241447e1a2715411cb3a60607cf04ff60809
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-19 23:39:05 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,26 @@
+function solution(array) {
+    var answer = 0;
+    let temp = {}
+    let cnt = 0;
+    for(i in array){
+        temp[array[i]] = 0
+    }
+    for(i in array){
+        temp[array[i]] += 1
+    }
+    let maxCnt = Math.max(...Object.values(temp))
+    for(i of Object.values(temp)){
+        if(maxCnt === i){
+            cnt++}
+    }
+    if(cnt > 1){
+        answer = -1
+    }else{
+        answer = Object.keys(temp)[Object.values(temp).indexOf(maxCnt)]
+    }
+    // console.log(Object.keys(temp))
+    // console.log(Object.values(temp))
+    // console.log(maxCnt)
+    // console.log(temp)
+    return +answer;
+}
