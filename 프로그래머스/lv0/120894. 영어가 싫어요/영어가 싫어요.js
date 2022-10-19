172c73f76b2d4bb8ccacc9a02a226324f92af795
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-20 03:18:11 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -15,3 +15,13 @@ function solution(numbers) {
     }
     return +answer;
 }
+
+// function solution(numbers) {
+//     const numMap = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }
+
+//     for(i in Object.entries(numMap)) {
+//         numbers = numbers.replaceAll(Object.keys(numMap)[i], Object.values(numMap)[i]);
+//     }
+
+//     return +numbers;
+// }
