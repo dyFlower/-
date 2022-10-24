2a8fc93a83c554e506d9288104c537ef9742941d
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-24 22:05:59 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,38 +1,12 @@
-// function solution(id_pw, db) {
-//     for(let i = 0; i < db.length; i++){
-//         if(id_pw[0] === db[i][0]){
-//             if(id_pw[1] == db[i][1]){
-//                 return 'login'
-//             }else{
-//                 return 'wrong pw'
-//             }
-//         }
-//     }
-//     return 'fail';
-// }
-function solution (id_pw, db) {
-    let result ='';
-    db.map(data => {
-        let checkedId = id_pw[0] === data[0]
-        let checkedPw = id_pw[1] === data[1]
-        // console.log(checkedId && checkedPw)
-        // console.log(checkedId && !checkedPw)
-        
-        console.log(!checkedId)
-          console.log('---')
-        console.log(!checkedId && !checkedPw)
-      
-
-        if(checkedId && checkedPw) {
-            result = 'login';
-       } 
-        else if (checkedId && !checkedPw) {
-            result = 'wrong pw'
-       }
-        else if(!checkedId && !checkedPw){
-            result = 'fail'
+function solution(id_pw, db) {
+    for(let i = 0; i < db.length; i++){
+        if(id_pw[0] === db[i][0]){
+            if(id_pw[1] == db[i][1]){
+                return 'login'
+            }else{
+                return 'wrong pw'
+            }
         }
-    })
-
-    return result
+    }
+    return 'fail';
 }
