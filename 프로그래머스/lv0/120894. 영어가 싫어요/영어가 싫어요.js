468a89738eb9e55d897d5d65cde479c2202ad778
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-24 11:29:21 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,27 +1,27 @@
-function solution(numbers) {
-    let temp = ['zero','one','two','three','four','five','six','seven','eight','nine']
-    numbers = numbers.split('')
-    var answer = '';
-    let box = ''
-    
-    for(i of numbers){
-        box += i
-        for(j in temp){
-            if(box === temp[j]){
-                answer += j
-                box = ''
-            }
-        }
-    }
-    return +answer;
-}
-
 // function solution(numbers) {
-//     const numMap = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }
-
-//     for(i in Object.entries(numMap)) {
-//         numbers = numbers.replaceAll(Object.keys(numMap)[i], Object.values(numMap)[i]);
+//     let temp = ['zero','one','two','three','four','five','six','seven','eight','nine']
+//     numbers = numbers.split('')
+//     var answer = '';
+//     let box = ''
+    
+//     for(i of numbers){
+//         box += i
+//         for(j in temp){
+//             if(box === temp[j]){
+//                 answer += j
+//                 box = ''
+//             }
+//         }
 //     }
-
-//     return +numbers;
+//     return +answer;
 // }
+
+function solution(numbers) {
+    const numMap = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }
+
+    for(i in Object.entries(numMap)) {
+        numbers = numbers.replaceAll(Object.keys(numMap)[i], Object.values(numMap)[i]);
+    }
+
+    return +numbers;
+}
