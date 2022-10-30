3534de986df9f6d8b2129002d06b8a52e51de538
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-10-30 10:48:54 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -10,10 +10,8 @@ function solution(left, right) {
             } //left에서 약수의 갯수를 계산
         }
         //약수에 갯수에 따라서 덧셈, 뺄셈을 진행
-        if(cnt % 2 === 0){
-            answer += left
-        }else{
-            answer -= left}
+        cnt % 2 === 0 ? answer += left : answer -= left
+        
         //다음 값을 탐색하기 위해 left에 1을 더하고, cnt를 0으로 초기화
         left++
         cnt = 0
