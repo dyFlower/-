a2c7f68f0745be6921c29a3394de7881a874bcd9
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-11-01 14:28:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,13 @@
+function solution(n, arr1, arr2) {
+    arr1 = arr1.map(v => v.toString(2).padStart(n,0))
+    arr2 = arr2.map(v => v.toString(2).padStart(n,0))
+    let arr3 = new Array(n).fill('')
+    for(let i in arr1){
+        for(let j in arr1[i]){
+            if(arr1[i][j] == 1 || arr2[i][j] == 1){
+                arr3[i] += '#'
+            }else{arr3[i] += ' '}
+        }
+    }
+    return arr3;
+}
