80739d84a5820391f44a0435b972080ceae87faa
Doyoung.Kim <112444362+dyFlower@users.noreply.github.com>
2022-11-29 10:29:45 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,13 +1,17 @@
 function solution(numlist, n) {
-    var answer = [];
-    numlist = numlist.sort((a,b) => b-a)
-    let closeNum = [...numlist].map(v => Math.abs(v - n))
-    for(i in numlist){
-        answer.push(numlist[closeNum.indexOf(Math.min(...closeNum))])
-        closeNum[closeNum.indexOf(Math.min(...closeNum))] = 100000
-    }
-    return answer;
+    return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
 }
+
+// function solution(numlist, n) {
+//     var answer = [];
+//     numlist = numlist.sort((a,b) => b-a)
+//     let closeNum = [...numlist].map(v => Math.abs(v - n))
+//     for(let i in numlist){
+//         answer.push(numlist[closeNum.indexOf(Math.min(...closeNum))])
+//         closeNum[closeNum.indexOf(Math.min(...closeNum))] = 100000
+//     }
+//     return answer;
+// }
 
 //다시 풀어보기!!
 // function solution(numlist, n) {
